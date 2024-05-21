78bdbb4070d2c5e073994a49cf91280c1e4f4acf
Enttony Cavalcante <enttonyfellipe@gmail.com>
2024-05-20 21:04:11 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,36 @@
+// Código do Carro
+
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [40, 96, 150, 210, 270, 318];
+let velocidadeCarros = [7, 6.9, 4.9, 5.8, 4.1, 3.3];
+let comprimentoCarro = 50;
+let alturaCarro = 40;
+
+
+
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], comprimentoCarro, alturaCarro);
+  } 
+}
+
+function movimentaCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    if (passouTodaATela(xCarros[i])){
+      xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarro){
+  return xCarro < -50;
+}
+
+
+
